cb7ab99d2cb975916937bd45752cc596e5c23015
Alejo <alejofacultad10@gmail.com>
2024-10-03 05:51:36 -0300

    first commit

---
+++ tp-js-dsw/main.js
@@ -0,0 +1,70 @@
+// INTRODUCCIÓN A JS
+
+console.log("Introducción a JS")
+
+// Ej2
+
+let a = 5;
+let b= 10;
+let c= a+b;
+console.log(`El resultado es: ${c}`);
+
+// ej3
+
+// let nombre = prompt("¿Cuál es tu nombre?");
+// console.log(`Hola, ${nombre}!`);
+
+// ---------------------------------------------------------------------------------------------
+
+// ej1
+
+let a1 = 1;
+let b1= 2;
+let c1= 3;
+
+if(a1>b1 && a1>c1){
+  console.log(`El mayor de los numeros es ${a1} `)
+}else if(b1>a1 && b1>c1){
+  console.log(`El mayor de los numeros es ${b1}`)
+}else{
+  console.log(`El mayor de los numeros es ${c1} `)
+}
+
+// ej2
+
+// let numero1= prompt(`Ingrese un número para ver si este es par o impar`)
+
+// if(numero1 %2==0){
+//   console.log("El número ingresado es par")
+
+// }else{
+//   console.log("El número ingresado es impar")
+// }
+
+// -----------------------------------------------------------------------------------
+
+// ej1
+
+let aa=10
+
+while (aa>0){
+  aa--
+  console.log(aa)
+}
+
+// ej2
+
+let bb;
+do{
+  bb= prompt(`Ingrese un número mayor a 100`)
+}while(bb<=100)
+
+  console.log(`El número ingresado es mayor a 100: ${bb}`)
+
+// -------------------
+
+// ej1
+
+
+
+

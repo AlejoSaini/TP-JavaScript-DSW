d4ee2ebaed889cb5c86d006f0757a72721562b16
Alejo <alejofacultad10@gmail.com>
2024-10-04 10:01:53 -0300

    finished

--- tp-js-dsw/main.js
+++ tp-js-dsw/main.js
@@ -81,16 +81,166 @@ console.log(`El número ${nroImpar} es par?: ${esPar(nroImpar)}`)
 
 // ej2
 
-const converturCelsiusAFarenheit=(grados)=>{
-  let calculo= grados *1.8+32
-  console.log(`${grados}°C son equivalentes a ${calculo}°F`)
-}
+// const converturCelsiusAFarenheit=(grados)=>{
+//   let calculo= grados *1.8+32
+//   console.log(`${grados}°C son equivalentes a ${calculo}°F`)
+// }
 
-let gradosIngresados = prompt("Ingrese los grados en Celsius a convertir")
-converturCelsiusAFarenheit(gradosIngresados)
+// let gradosIngresados = prompt("Ingrese los grados en Celsius a convertir")
+// converturCelsiusAFarenheit(gradosIngresados)
 
 // ------------------------------------
 
+// ej1
 
+let persona ={
+  nombre: "Ana",
+  edad:20,
+  ciudad:"Madrid",
 
+  cambiarCiudad: function(nuevaCiudad){
+    this.ciudad=nuevaCiudad
+  }
+}
+persona.cambiarCiudad("Barcelona")
 
+// ej2
+
+let libro ={
+  titulo:"El Quijote",
+  autor:"Cervantes",
+  anio: 1605,
+  esAntiguo: function() {
+    let anioActual = new Date().getFullYear;
+    let diferencia = anioActual - this.anio;
+    
+    if (diferencia > 10) {
+        console.log(`El libro "${this.titulo}" es antiguo: ${false}`);
+    } else {
+        console.log(`El libro "${this.titulo}" es antiguo: ${true}`);
+    }
+}
+}
+libro.esAntiguo();
+
+// ---------------------------------------------
+
+// ej1
+
+let numeros=[1,2,3,4,5,6,7,8,9,10]
+
+let numerosDuplicados=numeros.map((num)=>{
+  return num *2
+})
+console.log(`Números originales: ${numeros}`);
+console.log(`Números multiplicados por 2: ${numerosDuplicados}`);
+
+// ej2
+
+let pares=[]
+for(let i=1; i<=20; i++){
+  if(esPar(i)){
+    pares.push(i)
+  }
+}
+console.log(`Primeros 10 números pares: ${pares}`);
+
+// ------------------------------------------------------
+
+// ej1
+
+let button = document.getElementById("buttonColor");
+let text = document.getElementById("pText")
+const handleButton = () =>{
+  if(button.classList.contains('buttonBlue')){
+      button.classList.remove('buttonBlue')
+      button.classList.add('buttonBlack')
+  } else {
+    button.classList.remove('buttonBlack')
+    button.classList.add('buttonBlue')
+  }
+};
+const handleText = () =>{
+  if(text.classList.contains('textBlue')){
+      text.classList.remove('textBlue')
+  } else {
+    text.classList.add('textBlue')
+  }
+}
+button.addEventListener("click", () =>{
+  handleButton()
+  handleText()
+});
+
+// ej2
+
+let buttonForm = document.getElementById("buttonForm");
+buttonForm.addEventListener("click", () =>{
+  let inputForm = document.getElementById("inputForm").value;
+  alert(`Has ingresado: ${inputForm}`)
+})
+
+// ---------------------------------------------------------------------------------------------
+
+// ej1
+
+const liElements = document.querySelectorAll("li");
+liElements.forEach((element) => {
+  element.addEventListener("click", () => {
+    console.log(element.textContent)
+  })
+})
+
+// ej2
+
+const input = document.getElementById("input")
+const disabledbutton = document.getElementById("disabledButton")
+const enabledButton = document.getElementById("enabledButton")
+disabledbutton.addEventListener('click', () => {
+    input.disabled = true
+    disabledbutton.disabled = true
+    enabledButton.disabled = false
+});
+enabledButton.addEventListener('click', () => {
+    input.disabled = false
+    enabledButton.disabled = true
+    disabledbutton.disabled = false
+});
+enabledButton.disabled = true
+
+// ---------------------------------------------------------------------------------------------
+
+// ej1
+
+let buttonEmail = document.getElementById("buttonEmail")
+buttonEmail.addEventListener(("click"), ()=>{
+  let inputEmail = document.getElementById("inputEmail").value
+  localStorage.setItem("correo", JSON.stringify(inputEmail))
+});
+
+const renderEmail = () => {
+  const result = localStorage.getItem("correo")
+  console.log("LOCAL STORAGE");
+  if(result){
+      console.log(JSON.parse(result))
+  } else {
+      console.log("Los datos no existen en el LocalStorage")
+  }
+  if (result !== null){
+    (document.getElementById("savedEmail")).innerHTML = `
+    <p>
+      Correo guardado: ${JSON.parse(result)}
+    </p>
+    <button id="buttonDelete" class="buttonBlack">
+      Eliminar Correo Guardado
+    </button> 
+    `
+  }
+}
+renderEmail();
+
+let buttonDelete = document.getElementById("buttonDelete")
+buttonDelete.addEventListener(("click"), () =>{
+  localStorage.clear()
+  console.log('LocalStorage ha sido eliminado.')
+})
